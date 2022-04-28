b121ca20c31a2eb0472aa17812e12fcab94e81cf
thenameiskrishna <krishnasinghchauhan3@gmail.com>
2022-04-28 22:46:36 +0530

    os.js

---
+++ 28december/node/os.js
@@ -0,0 +1,9 @@
+const os = require('os')
+console.log(os.arch()) 
+// tells about Architecture(64 or 32)
+console.log(os.networkInterfaces())
+// Tells  everything about your network currently using 
+console.log(os.platform())
+// tells about system plateform
+console.log(os.cpus())
+// tells about your CPU specefication
